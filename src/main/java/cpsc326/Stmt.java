969165b6de786b06a7c1bc47efb9c7fa6ce7ddd6
KyleChoate <kyle7choate@gmail.com>
2026-04-17 15:47:17 -0700

    Updated Stmt

--- src/main/java/cpsc326/Stmt.java
+++ src/main/java/cpsc326/Stmt.java
@@ -6,17 +6,71 @@ abstract class Stmt
 {
   interface Visitor<R> 
   {
-    R visitStatement(Statement v);
+    R visitBlockStmt(Block stmt);
 
-    R visitVariableDeclaration(varDecl v);
+    R visitIfStmt(If stmt);
+
+    R visitPrintStmt(Print stmt);
+
+    R visitVarStmt(Var stmt);
+
+    R visitWhileStmt(While stmt);
   }
 
-  static class varDecl extends Stmt
+  static class Block extends Stmt
+  {
+    final List<Stmt> stmt;
+
+    Block(List<Stmt> stmt) 
+    {
+      this.stmt = stmt;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) 
+    {
+      return visitor.visitBlockStmt(this);
+    }
+  }
+
+  static class If extends Stmt
+  {
+    final Expr expr;
+
+    If(Expr expr) 
+    {
+      this.expr = expr;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) 
+    {
+      return visitor.visitIfStmt(this);
+    }
+  }
+
+  static class Print extends Stmt
+  {
+    final Expr expr;
+
+    Print(Expr expr) 
+    {
+      this.expr = expr;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) 
+    {
+      return visitor.visitPrintStmt(this);
+    }
+  }
+
+  static class Var extends Stmt
   {
     final Token name;
     final Expr expr;
 
-    varDecl(Token name, Expr expr) 
+    Var(Token name, Expr expr) 
     {
       this.name = name;
       this.expr = expr;
@@ -25,28 +79,28 @@ abstract class Stmt
     @Override
     <R> R accept(Visitor<R> visitor) 
     {
-      return visitor.visitVariableDeclaration(this);
+      return visitor.visitVarStmt(this);
     }
   }
 
-
-  static class Statement extends Stmt
+  static class While extends Stmt
   {
-    final Token name;
-    final Expr expr;
+    final Expr condition;
+    final Stmt body;
 
-    Statement(Token name, Expr expr) 
+    While(Expr condition, Stmt body) 
     {
-      this.name = name;
-      this.expr = expr;
+      this.condition = condition;
+      this.body = body;
     }
 
     @Override
     <R> R accept(Visitor<R> visitor) 
     {
-      return visitor.visitStatement(this);
+      return visitor.visitWhileStmt(this);
     }
   }
+
 
 
   abstract <R> R accept(Visitor<R> visitor);
